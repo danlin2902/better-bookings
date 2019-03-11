4a7e33f42639a5866f2b977d7d8fb2386d343856
Daniel Lindsey <daniel.lindsey321@gmail.com>
2019-03-11 16:32:33 +0100

    never commit in master anymore, I promise

---
+++ db/migrate/20190311151251_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[5.2]
+  def change
+    create_table :trips do |t|
+      t.integer :length
+      t.integer :cost
+      t.string :destination
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
