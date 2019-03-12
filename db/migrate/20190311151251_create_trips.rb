a85fdf847204cc8cb2dde836dcc04ee4c3d486e9
Daniel Lindsey <daniel.lindsey321@gmail.com>
2019-03-12 18:29:50 +0100

    la vita e bela

--- db/migrate/20190311151251_create_trips.rb
+++ db/migrate/20190311151251_create_trips.rb
@@ -1,7 +1,6 @@
 class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
-      t.integer :name
       t.integer :length
       t.integer :cost
       t.string :destination
