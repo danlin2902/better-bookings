73c787f5b61f227b3e34f10724af513cd10cb504
Daniel Lindsey <daniel.lindsey321@gmail.com>
2019-03-12 17:20:08 +0100

    updating before fetching master

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,10 @@
 class Trip < ApplicationRecord
   has_many :users, through: :bookings
 
-  mount_uploader :photo, PhotoUploader
+  validates :name, presence: true
+  validates :destination, presence: true
+  validates :cost, presence: true
+  validates :length, presence: true
 
+  mount_uploader :photo, PhotoUploader
 end
