8dae57860d960e0ef8bfa0423dd5eb3a77d04275
Regi Voda <vodaregi@gmail.com>
2019-03-12 10:35:45 +0100

    home almost done

--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,10 @@ ActiveRecord::Schema.define(version: 2019_03_11_151832) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "creator", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
