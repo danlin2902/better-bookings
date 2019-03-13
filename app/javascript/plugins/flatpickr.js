a02a80944d7f17edc4c6555e24a699f98a9df98b
andriy kravets <4kravets@gmail.com>
2019-03-13 13:47:11 +0100

    datepicker starts from today and end date cannot be less than today

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,19 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+const flatpickr = require("flatpickr");
+
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
+
+flatpickr(".datepicker", {
+  altInput: true,
+  enableTime: true,
+  minDate: "today",
+  maxDate: new Date().fp_incr(1), // 1 day from now
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+})
