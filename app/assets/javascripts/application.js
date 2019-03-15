74619d527bf247fa1e9be2291ca0bcf1f480f434
jacob coccari <jacob.coccari@gmail.com>
2019-03-15 14:11:02 +0100

    finished the homepage navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require rails-ujs
 //= require_tree .
 
-// request.env["exception_notifier.exception_data"] = {
-//       :current_user => User.first
-//     }
+
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,14 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-
+import { initUpdateNavbarOnScroll } from '../plugins/navbar';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
+
+
+initUpdateNavbarOnScroll();
 
 
 
